dcc57be42693fd2c040a0b46b58615671c5b0bd4
Rachel Suarez <105823797+raesuarez@users.noreply.github.com>
2023-08-01 14:20:57 -0400

    Update USERINFO.py

--- USERINFO.py
+++ USERINFO.py
@@ -1,4 +1,5 @@
 # after forking repository, please fill out and run main.py
 
+#copy paste the repository link between the ''
 #must be a valid repository link for a Javascript/npm project
 repo= ''
